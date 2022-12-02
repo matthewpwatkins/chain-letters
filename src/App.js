b2c964a683277ba29d98896c1d5b5e1932d60c21
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2022-12-01 23:02:10 -0600

    It's stupid, but it's on its way

--- src/App.js
+++ src/App.js
@@ -1,53 +1,52 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import SearchIcon from './search.svg';
-import MovieCard from './MovieCard';
-
-const API_URL = 'https://www.omdbapi.com?apikey=c032e2d7';
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [inputWord, setInputWord] = useState('');
+  const [linkWords, setLinkWords] = useState([]);
+  const [puzzle, setPuzzle] = useState({});
 
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-    setMovies(data.Search);
+  const load = () => {
+    setPuzzle({
+      sourceWord: 'please',
+      destinationWord: 'thank'
+    });
   };
 
+  const onSubmitWord = () => {
+    setLinkWords(l => {
+      const a = [...l];
+      a.push(inputWord);
+      return a;
+    });
+    setInputWord('');
+  }
+
   useEffect(() => {
-    searchMovies('The Chosen');
+    load();
   }, []);
 
   return (
-    <div className="app">
-      <h1>MovieLand</h1>
-      <div className='search'>
-        <input
-          placholder="Search for movies"
-          value={searchTerm}
-          onChange={(e) => { setSearchTerm(e.target.value) }}
-        />
-        <img
-          src={SearchIcon}
-          alt="Search"
-          onClick={() => { searchMovies(searchTerm) }}
-        />
-      </div>
-
-      {movies?.length > 0
-        ? (
-          <div className="container">
-            {movies.map((movie, index) => (
-              <MovieCard key={index} movie={movie} />
-            ))}
-          </div>
-        ) : (
-          <div className="empty">
-            <h2>No movies found</h2>
-          </div>
-        )}
-    </div>
+    <>
+      <h1>Title</h1>
+      {puzzle?.sourceWord ? (<>
+        <div>{puzzle.sourceWord}</div>
+        <div>
+          {linkWords.map(linkWord => (
+            <p key={linkWord}>{linkWord}</p>
+          ))}
+        </div>
+        <div>
+          <input
+            placholder="Search for movies"
+            value={inputWord}
+            onChange={(e) => { setInputWord(e.target.value) }}
+          />
+        </div>
+        <button onClick={onSubmitWord}>Add</button>
+        <div>{puzzle.destinationWord}</div>
+      </>) : (<></>)}
+    </>
   );
 };
 
