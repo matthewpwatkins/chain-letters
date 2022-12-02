fb3b48ef9e19488a5b83f576623b570197e8fc56
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2022-12-02 16:39:45 -0600

    Looking good so far

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { wordsAreCloseEnough, wordExists } from './WordJudge';
 const ALPHA_REGEX = /^[a-z]+$/i;
 
 const App = () => {
+  const [dateString, setDateString] = useState('');
   const [inputWord, setInputWord] = useState('');
   const [linkWords, setLinkWords] = useState([]);
   const [gameFinished, setGameFinished] = useState(false);
@@ -13,7 +14,9 @@ const App = () => {
   useEffect(() => { load(); }, []);
 
   const load = async () => {
-    const puzzlePath = `${process.env.PUBLIC_URL}/puzzles/${new Date().toISOString().substring(0, 10)}.json`;
+    const newDateString = new Date().toISOString().substring(0, 10);
+    setDateString(newDateString);
+    const puzzlePath = `${process.env.PUBLIC_URL}/puzzles/${newDateString}.json`;
     const res = await fetch(puzzlePath);
     const responsePuzzle = await res.json();
     console.group(responsePuzzle);
@@ -56,40 +59,58 @@ const App = () => {
       setInputWord('');
       if (inputWord === puzzle.destination_word) {
         setGameFinished(true);
-        alert(`You won in ${a.length} turns!`);
       }
       return a;
     });
   }
 
-  return (<>
-    <h1>Chain Letters</h1>
-    <div>
-      <span>{puzzle.source_word}</span>
-      &nbsp;&rarr;&nbsp;
-      <span>{puzzle.destination_word}</span>
-    </div>
-    <div>
-      {linkWords.map(linkWord => (
-        <p key={linkWord}>{linkWord}</p>
-      ))}
-    </div>
+  return (<div className='container-fluid app-container'>
+    <h1 className="display-5 mb-4 text-center">⛓️ Chain Letters 🔡</h1>
+    <div className="card my-3">
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item bg-primary text-light">
+          <span class="word-box">{puzzle.source_word}
+            &nbsp;<i class="fa-solid fa-arrow-right"></i>&nbsp;
+            {puzzle.destination_word}
+          </span>
+        </li>
+        {linkWords.map((linkWord, index) => {
+          let liClassName = "list-group-item p-1";
+          let indexClassName = "badge me-2";
+          if (gameFinished && index === linkWords.length - 1) {
+            liClassName += " bg-success text-light";
+            indexClassName += " text-bg-light";
+          } else {
+            indexClassName += " text-bg-secondary";
+          }
 
-    {(
-      gameFinished ? (<></>) : <div>
-        <input
-          value={inputWord}
-          onKeyUp={(e) => {
-            if (e.code === 'Enter') {
-              onSubmitWord();
-            }
-          }}
-          onChange={(e) => { setInputWord(e.target.value) }}
-        />
-        <button onClick={onSubmitWord}>Add</button>
-      </div>
-    )}
-  </>);
+          return (
+            <li key={linkWord} className={liClassName}>
+              <span class={indexClassName}>{index + 1}</span>
+              <span class="word-box">{linkWord}</span>
+            </li>
+          );
+        })}
+        {(gameFinished ? (<></>) : <li className="list-group-item p-0 border-0">
+          <div className="input-group">
+            <input
+              type="text"
+              class="form-control m-0 border-0"
+              value={inputWord}
+              placeholder="Next word..."
+              onKeyUp={(e) => { if (e.code === 'Enter') { onSubmitWord(); } }}
+              onChange={(e) => { setInputWord(e.target.value) }}
+            />
+            <button
+              type="button"
+              className="btn btn-info border-0"
+              style={{ borderRadius: 0 }}
+              onClick={onSubmitWord}>Add</button>
+          </div>
+        </li>)}
+      </ul>
+    </div>
+  </div >);
 };
 
 export default App;
