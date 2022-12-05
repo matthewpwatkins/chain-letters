687687672f459af93986c6d71b6fec0ad843d6c1
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2022-12-05 16:23:37 -0600

    Moved word functions to WordJudge module

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import { getUserPuzzle, storeUserPuzzle } from './StorageManager';
+import { wordExists, wordsAreCloseEnough } from './WordJudge';
 
 const ALPHA_REGEX = /^[a-z]+$/i;
 
 const App = () => {
-  const [userPuzzle, setUserPuzzle] = useState('');
-  const [activeLevel, setActiveLevel] = useState('easy');
-  const [activeLevelDefinition, setActiveLevelDefinition] = useState({});
-  const [activeLevelAttemptLinkWords, setActiveLevelAttemptLinkWords] = useState([]);
+  // UI state elements
   const [inputWord, setInputWord] = useState('');
   const [gameFinished, setGameFinished] = useState(false);
   const [showWinModal, setShowWinModal] = useState(false);
+
+  // Core data element
+  const [userPuzzle, setUserPuzzle] = useState('');
+
+  // TODO: all these are copmuted properties of the user puzzle itself. Can I eliminate them?
+  const [activeLevel, setActiveLevel] = useState('easy');
+  const [activeLevelDefinition, setActiveLevelDefinition] = useState({});
+  const [activeLevelAttemptLinkWords, setActiveLevelAttemptLinkWords] = useState([]);
+
 
   useEffect(() => {
     const lPadZeroNumber = (number, length) => {
@@ -45,72 +52,6 @@ const App = () => {
     };
     load();
   }, []);
-
-  // https://www.tutorialspoint.com/levenshtein-distance-in-javascript
-  const getLevenshteinDistance = (a, b) => {
-    const track = Array(b.length + 1)
-      .fill(null)
-      .map(() => Array(a.length + 1).fill(null));
-    for (let i = 0; i <= a.length; i += 1) {
-      track[0][i] = i;
-    }
-    for (let j = 0; j <= b.length; j += 1) {
-      track[j][0] = j;
-    }
-    for (let j = 1; j <= b.length; j += 1) {
-      for (let i = 1; i <= a.length; i += 1) {
-        const indicator = a[i - 1] === b[j - 1] ? 0 : 1;
-        track[j][i] = Math.min(
-          track[j][i - 1] + 1, // deletion
-          track[j - 1][i] + 1, // insertion
-          track[j - 1][i - 1] + indicator // substitution
-        );
-      }
-    }
-    return track[b.length][a.length];
-  };
-
-  const isSingleLetterSwap = (a, b) => {
-    if (a.length !== b.length) {
-      return false;
-    }
-
-    const diffIndices = [];
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        diffIndices.push(i);
-      }
-      if (diffIndices.length > 2) {
-        break;
-      }
-    }
-    if (diffIndices.length > 2) {
-      return false;
-    }
-    return a[diffIndices[0]] === b[diffIndices[1]]
-      && b[diffIndices[0]] === a[diffIndices[1]];
-  };
-
-  const wordsAreCloseEnough = (a, b) => {
-    const lDist = getLevenshteinDistance(a, b);
-
-    if (lDist === 1) {
-      // Single insertion or deletion
-      return true;
-    }
-
-    if (lDist === 2 && isSingleLetterSwap(a, b)) {
-      return true;
-    }
-
-    // Too many changes
-    return false;
-  };
-
-  const wordExists = async (word) => {
-    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-    return res.ok;
-  };
 
   const submitWord = async () => {
     const sanitizedInputWord = inputWord?.trim().toLocaleLowerCase();
