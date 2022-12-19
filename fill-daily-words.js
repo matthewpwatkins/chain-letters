3e019f22d17a364c39e4717bd0074222cff5f70a
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2022-12-19 17:12:39 -0600

    Words through the end fo the year

--- fill-daily-words.js
+++ fill-daily-words.js
@@ -25,7 +25,6 @@ const existingFileNames = fs.readdirSync(PUZZLE_DEFINITIONS_PATH).sort();
 const startDateString = existingFileNames.length ? existingFileNames[0].substring(0, 10) : todayDateString;
 const startDate = new Date(`${startDateString}T00:00:00Z`);
 
-const usedWords = new Set();
 const maxIterations = 365 * 10;
 
 for (let i = 0; i < maxIterations; i++) {
@@ -33,22 +32,15 @@ for (let i = 0; i < maxIterations; i++) {
   const currentDateString = currentDate.toShortIsoDateString();
   const fileName = `${currentDateString}.json`;
   const filePath = `${PUZZLE_DEFINITIONS_PATH}/${fileName}`;
-  if (fs.existsSync(filePath)) {
-    const puzzle = JSON.parse(fs.readFileSync(filePath));
-    usedWords.add(puzzle.source_word);
-    usedWords.add(puzzle.destination_word);
-  } else {
+  if (!fs.existsSync(filePath)) {
     const sourceWord = rl.question(`Source word for ${fileName}: `);
-    if (!sourceWord || usedWords.has(sourceWord)) {
+    if (!sourceWord) {
       throw new Error(sourceWord);
     }
-    usedWords.add(sourceWord);
-
     const destinationWord = rl.question(`Destination word for ${fileName}: `);
-    if (!destinationWord || usedWords.has(destinationWord)) {
+    if (!destinationWord) {
       throw new Error(destinationWord);
     }
-    usedWords.add(destinationWord);
 
     const puzzle = {
       source_word: sourceWord,
