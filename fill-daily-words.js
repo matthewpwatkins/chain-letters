340bf4774c01e71e9e717dd23989ff627f38d5af
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2022-12-28 15:44:33 -0600

    New words and fill words works better

--- fill-daily-words.js
+++ fill-daily-words.js
@@ -4,33 +4,16 @@ var rl = require('readline-sync');
 
 const PUZZLE_DEFINITIONS_PATH = 'public/puzzle-definitions';
 
-Date.prototype.addDays = function (days) {
-  let date = new Date(this.valueOf());
-  date.setDate(date.getDate() + days);
-  return date;
-}
-
-Date.prototype.toShortIsoDateString = function () {
-  return this.toISOString().substring(0, 10);
-}
-
-const now = new Date();
-const todayDateString = now.toShortIsoDateString();
-
 if (!fs.existsSync(PUZZLE_DEFINITIONS_PATH)) {
   fs.mkdirSync(PUZZLE_DEFINITIONS_PATH);
 }
 
-const existingFileNames = fs.readdirSync(PUZZLE_DEFINITIONS_PATH).sort();
-const startDateString = existingFileNames.length ? existingFileNames[0].substring(0, 10) : todayDateString;
-const startDate = new Date(`${startDateString}T00:00:00Z`);
+const fileNames = fs.readdirSync(PUZZLE_DEFINITIONS_PATH).sort();
+const nextNumber = parseInt(fileNames[fileNames.length - 1].substring(0, 5)) + 1;
 
-const maxIterations = 365 * 10;
-
-for (let i = 0; i < maxIterations; i++) {
-  const currentDate = startDate.addDays(i);
-  const currentDateString = currentDate.toShortIsoDateString();
-  const fileName = `${currentDateString}.json`;
+for (let i = 0; i < 100; i++) {
+  const number = nextNumber + i;
+  const fileName = `${(number + '').padStart(5, '0')}.json`;
   const filePath = `${PUZZLE_DEFINITIONS_PATH}/${fileName}`;
   if (!fs.existsSync(filePath)) {
     const sourceWord = rl.question(`Source word for ${fileName}: `);
