ee2f66bcfacddc7fd7cca2bdf681ead47f225eb3
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2023-01-16 16:41:22 -0600

    Added four new words, now with path

--- fill-daily-words.js
+++ fill-daily-words.js
@@ -16,18 +16,20 @@ for (let i = 0; i < 100; i++) {
   const fileName = `${(number + '').padStart(5, '0')}.json`;
   const filePath = `${PUZZLE_DEFINITIONS_PATH}/${fileName}`;
   if (!fs.existsSync(filePath)) {
-    const sourceWord = rl.question(`Source word for ${fileName}: `);
+    const sourceWord = rl.question(`${fileName} - source: `);
     if (!sourceWord) {
       throw new Error(sourceWord);
     }
-    const destinationWord = rl.question(`Destination word for ${fileName}: `);
+    const destinationWord = rl.question(`${fileName} - destination: `);
     if (!destinationWord) {
       throw new Error(destinationWord);
     }
+    const bestPath = rl.question(`${fileName} - best path ${sourceWord} => ${destinationWord}: `);
 
     const puzzle = {
       source_word: sourceWord,
-      destination_word: destinationWord
+      destination_word: destinationWord,
+      best_path: bestPath.split(',')
     };
     fs.writeFileSync(filePath, JSON.stringify(puzzle, undefined, '  '));
   }
